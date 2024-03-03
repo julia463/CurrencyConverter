474aecee5fd67182e0110c229ec66c364ad807a0
julia463 <54084784+julia463@users.noreply.github.com>
2024-03-02 20:41:59 -0800

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+    entry: {
+        index: "./src/index.tsx"
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+              test: /\.tsx?$/,
+               use: [
+                 {
+                  loader: "ts-loader",
+                   options: {
+                     compilerOptions: { noEmit: false },
+                    }
+                  }],
+               exclude: /node_modules/,
+            },
+            {
+              exclude: /node_modules/,
+              test: /\.css$/i,
+               use: [
+                  "style-loader",
+                  "css-loader"
+               ]
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "manifest.json", to: "../manifest.json" },
+            ],
+        }),
+        ...getHtmlPlugins(["index"]),
+    ],
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        path: path.join(__dirname, "dist/js"),
+        filename: "[name].js",
+    },
+};
+
+function getHtmlPlugins(chunks) {
+    return chunks.map(
+        (chunk) =>
+            new HTMLPlugin({
+                title: "React extension",
+                filename: `${chunk}.html`,
+                chunks: [chunk],
+            })
+    );
+}
+
+//SOURCE: https://medium.com/@tharshita13/creating-a-chrome-extension-with-react-a-step-by-step-guide-47fe9bab24a1
